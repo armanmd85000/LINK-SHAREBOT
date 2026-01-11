ac0495980879a8964340cdc0612ffa1e7d977183
armanmd85000 <154997050+armanmd85000@users.noreply.github.com>
2026-01-11 17:02:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+import asyncio
 from bot import Bot
 
-Bot().run()
+bot = Bot()
+
+async def main():
+    await bot.start()
+    await asyncio.Event().wait()  # keep bot alive forever
+
+if __name__ == "__main__":
+    asyncio.run(main())
